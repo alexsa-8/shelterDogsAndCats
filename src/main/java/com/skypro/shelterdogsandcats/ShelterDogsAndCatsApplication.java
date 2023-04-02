ed5253f5f1bfd259979391dae8e147a81517c63d
Alex <alexsa-8@mail.ru>
2023-04-02 15:45:08 +0300

    Приют собак и кошек

---
+++ src/main/java/com/skypro/shelterdogsandcats/ShelterDogsAndCatsApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.shelterdogsandcats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterDogsAndCatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterDogsAndCatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/shelterdogsandcats/ShelterDogsAndCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.shelterdogsandcats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterDogsAndCatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
